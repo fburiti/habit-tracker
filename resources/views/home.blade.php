3a8bc88dc535828657cd5d00ca4a168fe4b76cd2
fburiti <felisbertoburiti@hotmail.com>
2026-07-14 21:20:46 -0300

    feat: setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,27 +1,15 @@
-<h1> Bem vindo(a), você está na página home</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+<h1 class="text-32xl font-bold underline text-green-500">Bem-vindo ao {{ config('app.name') }}</h1>
 
-<p>
 
-  Olá, {{ $name }}
 
-</p>
-
-<p>
-Seus hábitos são:
-
-@foreach ($habits as $itens)
-
-<ul>
-<li>{{ $itens }}</li>
-</ul>
-
-@endforeach
-</p>
-
-@auth
-  <p>Você está logado</p>
-@endauth
-
-@guest
-   <p>Você não está logado</p>
-@endguest
+</body>
+</html>
